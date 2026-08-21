ae6d751de6cf78c36020d92a82b9b9a09b92ba04
nairx <nairxprv@gmail.com>
2026-08-21 15:55:26 +0530

    updated

---
+++ part1/index11.js
@@ -0,0 +1,7 @@
+//Object Spread Operator
+
+const product = {name:"Laptop",price:75000}
+
+const newObj = {...product,rating:5}
+
+console.log(newObj)
---
+++ part2/index12.js
@@ -0,0 +1,9 @@
+//Array of Objects
+
+const products = [
+    { id: 1, name: "Laptop", price: 75000 },
+    { id: 2, name: "Desktop", price: 25000 },
+    { id: 3, name: "Keyboard", price: 1000 }
+]
+
+//Print all the products
