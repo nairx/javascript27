a54522c16ae61930cfe29f59ad89f5290d26c846
nairx <nairxprv@gmail.com>
2026-08-21 16:32:01 +0530

    updated

--- part2/index12.js
+++ part2/index12.js
@@ -6,4 +6,44 @@ const products = [
     { id: 3, name: "Keyboard", price: 1000 }
 ]
 
-//Print all the products using forEach
+//Print names of all the products using forEach
+// products.forEach(product => console.log(product.name))
+
+//create a new array with all the products having price greater than 5000 then print the new array using filter
+// const newArr = products.filter(product => product.price > 5000)
+// console.log(newArr)
+
+//create a new array by increasing the price of product id 2 by 3000 using map
+// const newArr = products.map(product => {
+//     if (product.id === 2){
+//         return {...product,price:product.price+3000}
+//     }
+//     return product
+// })
+// console.log(newArr)
+
+//create a new array by increasing the price of product id 2 by 3000 using map
+// const newArr = products.map(product => product.id === 2 ? {...product,price:product.price+3000} : product)
+// console.log(newArr)
+
+let cart=[]
+//Write a function addToCart(id) to add the product into the cart using spread operator
+//Example addToCart(1) should add product 1 into the cart array and then print the cart
+//addToCart(1)
+//addToCart(3)
+function addToCart(id) {
+const product = products.find(product => product.id === id);
+if (product) {
+
+    cart = [...cart, {...product,quantity:1}];
+  }
+}
+console.log(cart);
+addToCart(1);
+console.log(cart);
+addToCart(3);
+console.log(cart);
+
+//print total Order Value
+ 
+
---
+++ part2/index13.js
@@ -0,0 +1,8 @@
+//Ternary operator
+
+let a = 10
+let b = 12
+a > b ? console.log("a is greater") : console.log("b is greater") 
+
+
+a > b ? console.log("a is greater") : b > a ? console.log("b is greater") : console.log("both are equal")
