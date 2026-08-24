d49956d7e32dc98931d3a794a6e9d680ed90a370
nairx <nairxprv@gmail.com>
2026-08-24 14:49:10 +0530

    updated

---
+++ part3/index27.js
@@ -0,0 +1,14 @@
+function outer(){
+    let count = 0
+    function inner(){
+        count++
+        console.log(count)
+    }
+    return inner
+}
+
+const counter = outer()
+
+counter()
+counter()
+counter()
